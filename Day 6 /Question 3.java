f0184a488c8c032ede87454caa7a368e925bfe87
Pranjal_dwivedi <pranjaldwivedi2005@gmail.com>
2026-06-30 08:04:19 -0700

    Add CountSetBits class to count set bits in a number

---
+++ Day 6 /Question 3.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class CountSetBits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        while (n > 0) {
+            count += n % 2;
+            n /= 2;
+        }
+
+        System.out.println("Set Bits = " + count);
+    }
+}
